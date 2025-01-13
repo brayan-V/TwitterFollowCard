8945a3e6c007a51b9fc98e9cca54fd3826d9bf4c
Brayan Velasco <bvelasco10b@gmail.com>
2025-01-13 17:55:49 -0500

    se usa un array con los datos en lugar de valores declarados directamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCardd";
 export function App() {
+  const users = [
+    {
+      userName: "midudev",
+      name: "Miguel Angel Duran",
+      initialIsFollowing: true
+    },
+    {
+      userName: "TMChein",
+      name: "Thomas Campo",
+      initialIsFollowing: false
+    },
+    {
+      userName: "pheralb",
+      name: "Pablo Hernandez",
+      initialIsFollowing: false
+    },
+    {
+      userName: "el",
+      name: "Elizabeth Len",
+      initialIsFollowing: true
+    },
+  ]
   return (
     <section className="App">
-      <TwitterFollowCard  userName="midudev" initialIsFollowing = {true}>
-        Miguel Angel Duran
-      </TwitterFollowCard>
-      <TwitterFollowCard  userName="pheralb">
-        Pablo Hernandez
-      </TwitterFollowCard>
-      <TwitterFollowCard  userName="el">
-        Elizabeth Len
-      </TwitterFollowCard>
+      {
+        users.map(({ userName, name, initialIsFollowing }) =>(
+          <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={initialIsFollowing}>
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
     </section>
   );
 }
